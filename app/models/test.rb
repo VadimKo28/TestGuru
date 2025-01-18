7fc358e752e45a665be5b677c4147b4a1704e1cb
Vadim Kozelskij <vadim.kozelskij@mail.ru>
2025-01-18 12:19:01 +0300

    fix: правки связи авторства пользователя для тестов

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
   has_many :tests_users
   has_many :users, through: :tests_users
   has_many :questions
