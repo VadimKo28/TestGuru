01df48527c6dcfcdb60b9796f9b5e36f7d8a407f
Vadim Kozelskij <vadim.kozelskij@mail.ru>
2025-03-13 00:24:47 +0300

    build: добавил локализацию в тесты пользователя и администратора

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Привет, #{resource.first_name}!"
@@ -11,9 +12,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { lang: I18n.locale } unless I18n.locale == params[:lang]
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
